039f72d35c8601bd9b52811b8fc3054e656ab8e0
Jiwon Sin <u5666033@anu.edu.au>
2021-11-10 18:03:11 +1100

    add project 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# Day 1 of the 100 day challenge
+# print('Day 1 - Python Print Function')
+# print('The function is declared like this:')
+# print('print(\'what to print\')')
+
+# Project 1
+def bandNameGenerator():
+    print("Hello this is name generator")
+    city_input = input("Which city did you grow up in?: ")
+    pet_name = input("What's the name of your pet?: ")
+    print("Your band name could be " + city_input + " " + pet_name)
+
+# Project 2 
