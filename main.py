42ded8df8b70833b4838b449e4c3729851a9c9b2
Jiwon Sin <u5666033@anu.edu.au>
2021-11-15 16:40:23 +1100

    day 2

--- main.py
+++ main.py
@@ -11,3 +11,31 @@ def bandNameGenerator():
     print("Your band name could be " + city_input + " " + pet_name)
 
 # Project 2 
+def dataTypeInteractive():
+    two_digit_number = input("Type in two digit number: ")
+    print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+def bmi_calculator():
+    height = float(input("Enter your height (m): "))
+    weight = float(input("Enter your weight (kg): "))
+    bmi = weight/height**2
+    print("Your BMI is: ", bmi)
+
+def calculate_age_left():
+    max_age = 90
+    age_input = int(input("How old are you?: "))
+    in_years = max_age-age_input
+    in_days = (in_years)*365
+    in_weeks = in_years*52
+    in_months = in_years*12
+    print(f"You have {in_days} days, {in_weeks} weeks and {in_months} months left")
+
+def tip_calculator():
+    print("Welcome to tip calculator")
+    total_bill = float(input("What was the total bill?: "))
+    tip_percentage = float(input("What percentage tip would you like to give? 10, 12, or 15?: "))
+    split_bill_count = int(input("How many people to split the bill?: "))
+    individual_payment = round(total_bill * (1+tip_percentage/100) / split_bill_count, 2)
+    print(f"Each person should pay: ${individual_payment}")
+
+tip_calculator()
