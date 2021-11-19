cfa8c8a5611c74bf21e4f1d99cd77db2907068db
Jiwon Sin <u5666033@anu.edu.au>
2021-11-20 01:27:56 +1100

    updating main.py day 3

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 # print('The function is declared like this:')
 # print('print(\'what to print\')')
 
-# Project 1
+# Day 1
 def bandNameGenerator():
     print("Hello this is name generator")
     city_input = input("Which city did you grow up in?: ")
     pet_name = input("What's the name of your pet?: ")
     print("Your band name could be " + city_input + " " + pet_name)
 
-# Project 2 
+# Day 2 
 def dataTypeInteractive():
     two_digit_number = input("Type in two digit number: ")
     print(int(two_digit_number[0]) + int(two_digit_number[1]))
@@ -38,4 +38,39 @@ def tip_calculator():
     individual_payment = round(total_bill * (1+tip_percentage/100) / split_bill_count, 2)
     print(f"Each person should pay: ${individual_payment}")
 
-tip_calculator()
+# Day 3
+def odd_or_even():
+    number = int(input("Which number do you want to change?: "))
+    if number % 2 == 0:
+        print("Even number!")
+    else:
+        print("Odd number")
+
+def bmi_version_two():
+    height = float(input("Enter your height in metres: "))
+    weight = float(input("Enter your weight in KG: "))
+    bmi_val = round(weight / height**2)
+    if bmi_val < 18.5:
+        print("You are UNDERWEIGHT")
+    elif bmi_val < 25:
+        print("You are in normal range")
+    elif bmi_val < 30:
+        print("You are Overweight")
+    elif bmi_val < 35:
+        print("You are obese")
+    else:
+        print("You are CLINICALLY OBESE")
+
+def leap_year():
+    year = int(input("Which year do you want to check?: "))
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year")
+        else:
+            print("Leap year")
+    else:
+        print("Not leap year")
+
+def pizza_order():
+    
