605ba93446b7edf77e5da6820bdb210af22ae07e
Jiwon Sin <u5666033@anu.edu.au>
2021-11-27 23:15:49 +1100

    adding new functions

--- hangman_game.py
+++ hangman_game.py
@@ -1,5 +1,28 @@
 import random
 
 word_list = ["aardvark", "baboon","camel"]
-random_word = word_list[random.randint(0, len(word_list) -1)]
+display = []
+chosen_word = word_list[random.randint(0, len(word_list) -1)]
+for i in range(len(chosen_word)):
+  display.append("_")
+print(display)
 
+user_guess = input("Guess a letter: ").lower()
+
+def show_word(word, user_letter):
+  
+
+def check_input(word, user_letter):
+  for letter in word:
+    if letter == user_letter:
+      return True
+  
+  return False
+
+print(chosen_word)
+
+guess = check_input(chosen_word, user_guess)
+if guess:
+  print("Correct Guess")
+else:
+  print("Wrong")
