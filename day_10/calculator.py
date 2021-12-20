4c9469d31523b17c4ec14cbe8f04be255f90a261
Jiwon Sin <u5666033@anu.edu.au>
2021-12-20 17:33:43 +1100

    making calculator app

---
+++ day_10/calculator.py
@@ -0,0 +1,17 @@
+# What's the first number?: 
+# + or - or * or /
+# Pick an operation
+# Next number?
+
+import calculator_art as art
+
+operation_dict
+
+print(art.logo)
+
+first_number = float(input("What's the first number?: "))
+
+operator = input("Pick an operation: ")
+
+second_number = float(input("What's the next number?: "))
+
