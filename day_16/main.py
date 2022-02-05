1ac5afd3f6faa5f2cf487cd95e95407d3fc9e490
Jiwon Sin <u5666033@anu.edu.au>
2022-02-05 23:47:53 +1100

    Finished Day 16

--- day_16/main.py
+++ day_16/main.py
@@ -1,1 +1,18 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+mm = MoneyMachine()
+cm = CoffeeMaker()
+menu = Menu()
+
+while True:
+    coffee_choice = input(f"What would you like? ({menu.get_items()}): ")
+
+    if coffee_choice == 'report':
+        cm.report()
+        mm.report()
+    elif cm.is_resource_sufficient(menu.find_drink(coffee_choice)):
+        print("Please insert coins.")
+        if mm.make_payment(menu.find_drink(coffee_choice).cost):
+            cm.make_coffee(menu.find_drink(coffee_choice))
